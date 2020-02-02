06a60a07ebc7e9d3daf8d1ef4b2a0721f28a1cf6
unknown <Lus Eduardo>
2020-02-02 19:26:21 -0300

    Front-end

---
+++ main.js
@@ -0,0 +1,79 @@
+var apilink = 'http://api-luised-dev-com.umbler.net/api/videos'
+
+async function render(){
+    const axi = await axios.get(apilink);
+    for (data of axi.data){ 
+        let { _id, title, link, like = 0, deslike = 0 } = data
+        var lista = document.getElementById('list')
+
+        let divsVerticais = document.createElement('div')
+        divsVerticais.setAttribute('class', 'divsVerticais')
+
+        let divInterna = document.createElement("div")
+        divInterna.setAttribute("class", "interno")
+
+        let youtube = document.createElement("iframe")
+        youtube.setAttribute("id", "ytplayer")
+        youtube.setAttribute("type", "text/html")
+        youtube.setAttribute("src", `https://www.youtube.com/embed/${link}`)
+        youtube.setAttribute("frameborder", "0")
+
+        let linkElement = document.createElement("a")
+        linkElement.setAttribute("target", "_blank")
+        linkElement.setAttribute("href", `https://youtu.be/${link}`)
+        linkElement.setAttribute("class", "text")
+        linkElement.appendChild(document.createTextNode(title))
+
+        let contadorLikes = document.createElement('h1')
+        contadorLikes.setAttribute("class", "contadores")
+        contadorLikes.setAttribute("id", "likes")
+        contadorLikes.appendChild(document.createTextNode(like))
+
+        let buttonLike = document.createElement("button")
+        buttonLike.setAttribute("onClick", `vote(this.id, "${_id}")`)
+        buttonLike.setAttribute("class", "btn")
+        buttonLike.setAttribute("id", "like")
+        buttonLike.appendChild(document.createTextNode("✅"))
+
+        let contadorDeslikes = document.createElement('h1')
+        contadorDeslikes.setAttribute("class", "contadores")
+        contadorDeslikes.setAttribute("id", "deslikes")
+        contadorDeslikes.appendChild(document.createTextNode(deslike))
+
+        let buttonDeslike = document.createElement("button")
+        buttonDeslike.setAttribute("onClick",`vote(this.id, "${_id}")`)
+        buttonDeslike.setAttribute("class", "btn")
+        buttonDeslike.setAttribute("id", "deslike")
+        buttonDeslike.appendChild(document.createTextNode("❌"))
+        
+        divInterna.appendChild(youtube)
+        divInterna.appendChild(linkElement)
+
+        divInterna.appendChild(contadorLikes)
+        divInterna.appendChild(buttonLike)
+        divInterna.appendChild(contadorDeslikes)
+        divInterna.appendChild(buttonDeslike)
+
+        divsVerticais.appendChild(divInterna)
+
+        lista.appendChild(divsVerticais)
+
+    };
+}
+
+async function vote(option, id){
+    console.log(id)
+    let get = await axios.get(`${apilink}/${id}`)
+    let { like = 0, deslike = 0 } = get.data
+    if(option == "like"){
+        like += 1;
+        const axi = await axios.put(`${apilink}/${id}`, {"like": like})
+        window.location.reload();
+    } else if(option == "deslike"){
+        deslike += 1;
+        const axi = await axios.put(`${apilink}/${id}`, {"deslike": deslike})
+        window.location.reload();
+    }
+}
+
+render()
